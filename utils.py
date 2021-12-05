d7b47e4c1c6ed6dfcb246e8330b37ede1584da6f
ddp-danilo <danilo.ddp1998@gmail.com>
2021-12-05 10:16:05 -0300

    Adicionado ao utils uma funcão para exibir todos as pessoas do banco de dados.

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-from models import Pessoas
+from models import Pessoas, Atividades
 
 # Insere dados na tabela pessoa
 def insere_pessoas():
-    pessoa = Pessoas(nome='das',idade=15)
+    pessoa = Pessoas(nome='dan',idade=21)
     print(pessoa)
     pessoa.save()
 # Realiza consulta na tabela pessoa
@@ -23,8 +23,21 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Dan').first()
     pessoa.delete()
 
+# Lista no terminal todas as atividades da base de dados #Use com cuidado.
+def lista_atividades():
+    atividades = Atividades.query.all()
+    print([{'id':i.id, 'nome':i.nome, 'pessoa_id':i.pessoa_id,'status': i.status} for i in atividades])
+
+# Lista no terminal todas as pessoas no banco de dados. #Use com cuidado.
+def lista_pessoas():
+    pessoas = Pessoas.query.all()
+    print([{'id': i.id, 'nome': i.nome, 'idade': i.idade } for i in pessoas])
+
+
 if __name__=='__main__':
     #insere_pessoas()
-    exclui_pessoa()
-    consulta_pessoas()
+    #exclui_pessoa()
+    #consulta_pessoas()
     #altera_pessoa()
+    #lista_atividades()
+    #lista_pessoas()
